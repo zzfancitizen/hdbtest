de82fbda05381c8a87c3206bfb5cb9740831b62e
zzfancitizen <zzfancitizen@gmail.com>
2017-03-30 11:43:52 +0800

    manifest.yml

--- wrapperServer.js
+++ wrapperServer.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const cron = require('cron');
-const hdb = require('./scadaWrapper').myHDB;
+const hdbScada = require('./scadaWrapper').myHDB;
+const hdbEqui = require('./equiWrapper').myHDB;
 const async = require('async');
 
 var server = http.createServer().listen(process.env.PORT || 8080);
 var CronJob = cron.CronJob;
-var scheduelWrap = {};
-var isRunning = false;
+var scheduleScada = {};
+var scheduleEqui = {};
+var isRunningScada = false;
+var isRunningEqui = false;
 
 server.on('request', function (req, res) {
     if (req.url == '/scada') {
@@ -23,15 +26,15 @@ server.on('request', function (req, res) {
                     .replace(/%3/g, time[0])
                     .replace(/%4/g, JSON.parse(chunk.toString()).freq);
                 time = [];
-                if (!isRunning) {
-                    hdb.hdbconnect();
-                    scheduleWrap = new CronJob(rule, function () {
-                        hdb.setParams(JSON.parse(chunk.toString()).time, JSON.parse(chunk.toString()).freq);
-                        hdb.hdbTrans();
+                if (!isRunningScada) {
+                    hdbScada.hdbconnect();
+                    scheduleScada = new CronJob(rule, function () {
+                        hdbScada.setParams(JSON.parse(chunk.toString()).time, JSON.parse(chunk.toString()).freq);
+                        hdbScada.hdbTrans();
                         console.log('Job run');
                     }, null, false, null);
-                    scheduleWrap.start();
-                    isRunning = true;
+                    scheduleScada.start();
+                    isRunningScada = true;
                     res.writeHead(200, {'Content-Type': 'test/plain'});
                     res.write('Job scheduled');
                     res.end();
@@ -45,10 +48,10 @@ server.on('request', function (req, res) {
         ;
         if (req.method == 'DELETE') {
             try {
-                hdb.hdbClose();
-                scheduleWrap.stop();
-                isRunning = false;
-                scheduleWrap = {};
+                hdbScada.hdbClose();
+                scheduleScada.stop();
+                isRunningScada = false;
+                scheduleScada = {};
                 res.writeHead(200, {'Content-Type': 'test/plain'});
                 res.write('Job cancelled');
                 res.end();
@@ -59,7 +62,51 @@ server.on('request', function (req, res) {
             }
         }
         ;
-    } else {
+    } else if (req.url == '/equi') {
+        if (req.method == 'GET') {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(__dirname);
+            res.end();
+        }
+        if (req.method == 'PUT') {
+            req.on('data', function (chunk) {
+                if (!isRunningEqui) {
+                    hdbEqui.hdbconnect();
+                    scheduleEqui = new CronJob('0 0 0 * * *', function () {
+                        hdbEqui.hdbTrans();
+                        console.log('Job run');
+                    }, null, false, null);
+                    scheduleEqui.start();
+                    isRunningEqui = true;
+                    res.writeHead(200, {'Content-Type': 'test/plain'});
+                    res.write('Job scheduled');
+                    res.end();
+                } else {
+                    res.writeHead(200, {'Content-Type': 'test/plain'});
+                    res.write('Job already scheduled');
+                    res.end();
+                }
+            })
+        }
+        ;
+        if (req.method == 'DELETE') {
+            try {
+                hdbEqui.hdbClose();
+                scheduleEqui.stop();
+                isRunningEqui = false;
+                scheduleEqui = {};
+                res.writeHead(200, {'Content-Type': 'test/plain'});
+                res.write('Job cancelled');
+                res.end();
+            } catch (err) {
+                res.writeHead(500, {'Content-Type': 'test/plain'});
+                res.write('Please schedule one job first.');
+                res.end();
+            }
+        }
+        ;
+    }
+    else {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.write('Not found');
         res.end();
